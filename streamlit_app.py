0e0682b7f0235b6ef857047a2cbb7e184e9bffbb
adarshtripathisnowflake <124794187+adarshtripathisnowflake@users.noreply.github.com>
2023-02-08 19:18:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Blueberry Oatsmeal')
+streamlit.text('Brown Bread with Peanut Butter')
+streamlit.text('Salad with paneer')
